410e05986d720ea4ec930f3738877ab64ed4f702
kirito <48683351+jonatha5858@users.noreply.github.com>
2022-03-29 19:00:49 -0300

    concluido (acho)

---
+++ AT-Task01/README.md
@@ -0,0 +1,19 @@
+# Compiladores
+
+
+Roteiro:
+
+- rever a Aula 07 sobre notacao posfixa [reverse polish notation]
+
+- Implementar uma linguagem RPN stacker em Java usando uma pilha como estrutura de dados
+
+- Programa le um arquivo com a expressao em RPN e avaliar
+
+Exemplo de entrada:
+
+10
+10
++
+Saida: 20
+
+ARQUIVO EXEMPLO: [(Arquivo EX (Calc1.stk)]
---
+++ AT-Task01/RPN.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RPN {
+    private Stack<Integer> pilha;
+
+    public RPN(){
+            pilha = new Stack<Integer>();
+            }
+    public static void main(String[] args) throws Exception{
+        String expressao;
+        int resultado;
+        RPN avaliador = new RPN();
+        expressao = "";
+        //alterar o path para o do arquivo que possuir a expressao a ser analisada. 
+        FileReader fr = new FileReader("C:\\Users\\User\\OneDrive\\Área de Trabalho\\AT-Task01\\AT-Task01\\Entrada.stk");
+        BufferedReader lerArq = new BufferedReader(fr);
+        String linha = lerArq.readLine();
+        while (linha != null) {
+            expressao += linha + " ";
+            linha = lerArq.readLine(); //
+          }
+        resultado = avaliador.avaliar(expressao);
+        System.out.println(resultado);
+   }
+	public int avaliar(String expr){
+            int op1, op2, resultado = 0;
+            String token;
+            Scanner in = new Scanner(expr);
+
+            while (in.hasNext())        
+            {
+                token = in.next();          
+
+                if (isOperador(token))
+                {
+                    op2 = (pilha.pop()).intValue();
+                    op1 = (pilha.pop()).intValue();
+                    resultado = avaliarOperador(token.charAt(0), op1, op2);     //
+                    pilha.push(resultado);
+                }
+                else
+                	pilha.push(Integer.parseInt(token));       
+            }
+
+            return resultado;
+        }
+
+     private boolean isOperador(String token){
+            return ( token.equals("+") || token.equals("-") ||
+                     token.equals("*") || token.equals("/") || token.equals("%") );
+
+        }
+     private int avaliarOperador(char operacao, int op1, int op2){
+            int resultado = 0;
+
+            switch (operacao)
+            {
+                case '+':
+                    resultado = op1 + op2;
+                    break;
+                case '-':
+                    resultado = op1 - op2;
+                    break;
+                case '*':
+                    resultado = op1 * op2;
+                    break;
+                case '/':
+                    resultado = op1 / op2;
+                    break;
+                case '%':
+                    resultado = op1 % op2;
+                    break;
+            }
+            return resultado;
+        }
+}
